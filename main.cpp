4dadea2d6678fd57b708f087c7af438c17a85c7e
Ahmed El Mansy <ahmedmansi2004@gmail.com>
2025-09-04 22:44:02 +0300

    another setup

--- main.cpp
+++ main.cpp
@@ -1,15 +1,35 @@
-#include <SDL2/SDL.h>
+#include "src/include/SDL.h"
+#include <stdio.h>
 
 const int WIDTH = 800, HEIGHT = 600;
 
-int main( int argc, char* argv[] ){
+int main() {
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
 
-    SDL_Window* window = nullptr;
-    SDL_Renderer* renderer = nullptr;
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
 
-    SDL_Init(SDL_INIT_EVERYTHING);
+    if (SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &window, &renderer) != 0) {
+        printf("SDL_CreateWindowAndRenderer Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
 
-    window = SDL_CreateWindow("Hello SDL World");
-    
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderDrawPoint(renderer, WIDTH / 2, HEIGHT / 2);
+
+    SDL_RenderPresent(renderer);
+    SDL_Delay(3000); // show for 3 seconds
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
